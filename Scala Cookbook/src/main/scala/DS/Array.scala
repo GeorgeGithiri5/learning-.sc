7c31ec959638eb4e26de3f3fe4ba5949751ebcf7
GeorgeGithiri5 <georgegithiri002@gmail.com>
2021-11-25 21:18:06 +0300

    Scala Cookbook

---
+++ Scala Cookbook/src/main/scala/DS/Array.scala
@@ -0,0 +1,16 @@
+package DS
+
+object ArrayObj
+{
+  def main(args: Array[String]): Unit =
+  {
+    var days = Array("Sunday",
+      "Monday", "Tuesday", "Wednesday", "Thursday",
+      "Friday")
+    println("Array elements are : ")
+    for(m1<-days)
+      {
+        println(m1)
+      }
+  }
+}
---
+++ Scala Cookbook/src/main/scala/DS/Map.scala
@@ -0,0 +1,15 @@
+package DS
+
+object GFGGG
+{
+  def main(args: Array[String])
+  {
+    val mapIm = Map("Ajay" -> 30,
+      "Bhavesh" -> 20,
+      "Charlie" -> 50)
+
+    // Accessing score of Ajay
+    val ajay = mapIm("Bhavesh")
+    println(ajay)
+  }
+}
