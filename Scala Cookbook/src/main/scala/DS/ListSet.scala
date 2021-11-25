3a301d3c7bec08f5d1503019048d83881966dd74
GeorgeGithiri5 <georgegithiri002@gmail.com>
2021-11-25 20:40:43 +0300

    Scala Cookbook

--- Scala Cookbook/src/main/scala/DS/ListSet.scala
+++ Scala Cookbook/src/main/scala/DS/ListSet.scala
@@ -1,6 +1,19 @@
 package DS
 
-object ListSet
-{
+// Scala program to Initialize a ListSet
+import scala.collection.immutable._
 
+// Creating object
+object GFGG
+{
+  // Main method
+  def main(args:Array[String])
+  {
+    println("Initializing an immutable ListSet ")
+
+    // Creating ListSet
+    val listSet1: ListSet[String] = ListSet("GeeksForGeeks",
+      "Article", "Scala")
+    println(s"Elements of listSet1 = $listSet1")
+  }
 }
